d488fb98c9fd224fc8d06345c22b6e5473f22aad
kattababu@gmail.com <Mohanrao@>
2017-04-12 11:50:13 +0530

    Updated Code

--- TeleAmazonas/src/com/Nutch/Crawl/TelAmz/TeleAmazonasMain.java
+++ TeleAmazonas/src/com/Nutch/Crawl/TelAmz/TeleAmazonasMain.java
@@ -26,7 +26,8 @@ public class TeleAmazonasMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 new TeleAmazonasCNT().TelAmzCNT();
-		
+
+			SplitFiles.FileSplitS();
 
 	}
 
